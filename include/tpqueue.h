4ddc7ed910296a59f0e531cd71559dd675e725ad
SharandovaOlga <75723905+SharandovaOlga@users.noreply.github.com>
2022-06-09 19:27:19 +0300

    Sharandova ADS-7

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,8 +4,73 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+  struct ITEM {
+    T value;
+    ITEM *next;
+    ITEM *prev;
+  };
+  
+public:
+  T pop();
+  void push(const T& value);
+  TPQueue():head(nullptr), tail(nullptr) {}
+  TPQueue() {
+    while (head)
+      pop;
+  }
+private:
+  TPQueue()::ITEM *create(const T& value) {
+    ITEM *item = new ITEM;
+    item->value = value;
+    item->next = nullptr;
+    item->prev = nullptr;
+    return item;
+  }
+  ITEM *tail;
+  ITEM *head;
 };
+
+template<typename T>
+void TPQueue<T>::push(const T& value) {
+  ITEM *temp = head;
+  ITEM *item = craete(value);
+  while (temp && temp->value.prior >= value.prior)
+    temp = temp->next;
+  if (!temp && head){
+    tail->next = item;
+    tail->next->prev = tail;
+    tail = item;
+  } else if (!temp && !head) {
+    head = tail = item:
+  } else if (!temp->prev) {
+    temp->prev = item;
+    item->next = temp;
+    head = item;
+  } else {
+    temp->prev->next = item;
+    item->prev = temp->prev;
+    item->next = temp;
+    temp->prev = item;
+  }
+}
+
+template<typename T>
+T TPQueue<T>::pop() {
+  if (head) {
+    ITEM *temp =head->next;
+    if (temp)
+      temp->prev = nullptr;
+    T value = head->value;
+    delete head;
+    if (!head)
+      tail = nullptr;
+    head = temp;
+    return value;
+  } else {
+    throw std::string("Empty!");
+  }
+}
 
 struct SYM {
   char ch;
